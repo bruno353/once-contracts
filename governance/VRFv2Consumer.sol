e848d09abe50345b048161725647008244ea9184
Bruno Santos <brunolsantos152@gmail.com>
2022-07-01 20:28:32 -0300

    Update VRFv2Consumer.sol

--- governance/VRFv2Consumer.sol
+++ governance/VRFv2Consumer.sol
@@ -4,21 +4,31 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./stakeable.sol";
+import "./OnceToken.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract VRFv2Consumer is VRFConsumerBaseV2 {
+
+
+contract OnceOracle is VRFConsumerBaseV2 {
   VRFCoordinatorV2Interface COORDINATOR;
+
+  //VRF CONSUMER:
 
   // Your subscription ID.
   uint64 s_subscriptionId;
 
   // Rinkeby coordinator. For other networks,
   // see https://docs.chain.link/docs/vrf-contracts/#configurations
-  address vrfCoordinator = 	0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed;
+  address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
 
   // The gas lane to use, which specifies the maximum gas price to bump to.
   // For a list of available gas lanes on each network,
   // see https://docs.chain.link/docs/vrf-contracts/#configurations
-  bytes32 keyHash = 0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+  bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
 
   // Depends on the number of requested values that you want sent to the
   // fulfillRandomWords() function. Storing each word costs about 20,000 gas,
@@ -39,9 +49,10 @@ contract VRFv2Consumer is VRFConsumerBaseV2 {
   uint256 public s_requestId;
   address s_owner;
 
-  constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
+  constructor(uint64 subscriptionId, ERC20Stakeable _token) VRFConsumerBaseV2(vrfCoordinator) {
     COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
     s_owner = msg.sender;
+    tokenERC20 = _token;
     s_subscriptionId = subscriptionId;
   }
 
@@ -56,12 +67,27 @@ contract VRFv2Consumer is VRFConsumerBaseV2 {
       numWords
     );
   }
+  uint256 public numero;
   
   function fulfillRandomWords(
-    uint256, /* requestId */
+    uint256 requestId, /* requestId */
     uint256[] memory randomWords
   ) internal override {
+    uint256 _tokenId = s_requestIdToTokenId[requestId];
+    (,,,,,address insurer,,) = tokenERC721.Items(_tokenId);
+    string memory ownerData = tokenERC721.fetchURI(insurer);
     s_randomWords = randomWords;
+    _insuranceRequestsIds.increment();
+    address[] memory _assessors = new address[](9);
+    _assessors[0] = 0xCa7168B179f474eFb8AAE82553Eb98bD20b7b722;
+    InsRequestIdToInsRequests[_insuranceRequestsIds.current()] = InsuranceRequest({
+          requester: msg.sender,
+          insurerData: ownerData,
+          timeOfRequest: block.timestamp,
+          numberOfRequests: 1,
+          assessors: _assessors
+        });
+    numero = s_randomWords[0];
   }
 
   modifier onlyOwner() {
@@ -72,4 +98,86 @@ contract VRFv2Consumer is VRFConsumerBaseV2 {
   function setOwner(address _address) public onlyOwner() {
     s_owner = _address;
   }
+
+
+
+
+  //ORACLE:
+
+  using Counters for Counters.Counter;
+  Counters.Counter public _insuranceRequestsIds;
+
+  event InsuranceRequested(uint256, uint256) ;
+
+  struct InsuranceRequest {
+    address requester;
+    string insurerData;
+    uint256 timeOfRequest;
+    uint256 numberOfRequests;
+    address[] assessors;
+  }
+  mapping(uint256 => InsuranceRequest) InsRequestIdToInsRequests;
+  mapping(uint256 => uint256) s_requestIdToTokenId;
+
+    ERC20Stakeable public tokenERC20;
+    OnceToken public tokenERC721;
+
+    function setTokenERC20(ERC20Stakeable _token) public onlyOwner() {
+        
+        tokenERC20 = _token;
+    }
+
+    function setTokenERC721(OnceToken _token) public onlyOwner(){
+        
+        tokenERC721 = _token;
+    }
+
+
+
+    modifier NFTOwner(uint256 _tokenId){
+        require(tokenERC721.ownerOf(_tokenId) == msg.sender, "Only the NFT owner can claim an insurance");
+        _;
+    }
+
+
+    uint256 public number;
+
+
+    function claimInsurance(uint256 _tokenId) public NFTOwner(_tokenId) {
+
+        (,,,,,address insurer,,) = tokenERC721.Items(_tokenId);
+        string memory ownerData = tokenERC721.fetchURI(insurer);
+        _insuranceRequestsIds.increment();
+        
+        s_requestId = COORDINATOR.requestRandomWords(
+          keyHash,
+          s_subscriptionId,
+          requestConfirmations,
+          callbackGasLimit,
+          numWords
+        );
+
+        s_requestIdToTokenId[s_requestId] = _tokenId;
+        emit InsuranceRequested(s_requestId, _tokenId);
+
+    }
+    
+            //InsRequestIdToInsRequests[_insuranceRequestsIds.current()] = InsuranceRequest({
+          //requester: msg.sender,
+         // insurerData: ownerData,
+          //timeOfRequest: block.timestamp,
+         // numberOfRequests: 1,
+          //assessors: [0xd9145CCE52D386f254917e481eB44e9943F39138]
+        //});
+
+        //Randomly select 9 assessors for the claiming inquiry
+
+        //address[] memory assessor;
+        //for (uint i = 0; i < tokenERC20.getStakersArrLength; i++) {
+            //address _assessor = tokenERC20.stakersArr(i);
+            //if(tokenERC20.stakers[_assessor]){
+                
+            //}
+
+
 }
